5fe88e66e0d558070865e4b368e6499e94703b69
dude <vovan_trc@mail.ru>
2017-08-08 23:29:08 +0300

    Простой REST сервис

---
+++ webpart/src/main/java/ru/dude/servak/minima/servlets/rest/SomeRest.java
@@ -0,0 +1,8 @@
+package ru.dude.servak.minima.servlets.rest;
+
+/**
+ * Created by dude on 07.08.2017.
+ */
+public interface SomeRest {
+    String ping();
+}
---
+++ webpart/src/main/java/ru/dude/servak/minima/servlets/rest/SomeRestImpl.java
@@ -0,0 +1,24 @@
+package ru.dude.servak.minima.servlets.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+
+/**
+ * Пример REST-сервиса
+ * Вызывается адресом http://localhost:8080/xxx/webapi/rest/ping
+ * Created by dude on 07.08.2017.
+ */
+@Path("/rest")
+@ApplicationPath("/webapi")
+public class SomeRestImpl extends Application implements SomeRest{
+
+    @GET
+    @Produces("text/html")
+    @Path("/ping")
+    public String ping() {
+        return "<html lang=\"en\"><body><h1>Hello, I'm REST html!!</body></h1></html>";
+    }
+}
